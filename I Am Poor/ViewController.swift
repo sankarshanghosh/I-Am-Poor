17086037601ed4c7d96e60c5383ef5eb6b5651e7
Sankarshan Ghosh <50132419+sankarshanghosh@users.noreply.github.com>
2020-02-25 16:50:10 +0530

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Sankarshan Ghosh on 25/02/20.
+//  Copyright © 2020 Sankarshan Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
